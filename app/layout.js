15fa101fe9ec0e36e5edb4be17208022d71962b7
Alex Stroev <stroev.alexander@gmail.com>
2023-07-21 11:55:54 +0300

    fontz fix

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,14 @@
 import "./scss/globals.scss";
+import { Ubuntu } from "next/font/google";
+const ubuntu = Ubuntu({
+  weight: ["400", "700"],
+  subsets: ["cyrillic", "latin"],
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ru-RU">
-      <body>{children}</body>
+      <body className={ubuntu.className}>{children}</body>
     </html>
   );
 }
