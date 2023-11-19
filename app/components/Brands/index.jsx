11a8d7a1a334578434ce8d1e5afc12c31b3cd629
Alex Stroev <stroev.alexander@gmail.com>
2023-11-19 16:47:53 +0300

    +

--- app/components/Brands/index.jsx
+++ app/components/Brands/index.jsx
@@ -5,62 +5,67 @@ import { Kernel } from "../ui";
 import brandsContent from "./brandsContent";
 import BrandCard from "./BrandCard";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
-// 
+import { Pagination, Navigation } from "swiper/modules";
+//
 import "swiper/css";
-import 'swiper/css/pagination';
+import "swiper/css/pagination";
 
 // https://swiperjs.com/demos#pagination-custom - демки свипера
 
-
 const Brands = () => {
-  const isDesktop = useMediaQuery("(min-width: 960px)");
+    const isDesktop = useMediaQuery("(min-width: 960px)");
 
-  const brandsList = Object.keys(brandsContent);
+    const brandsList = Object.keys(brandsContent);
 
-  const pagination = {
-    clickable: true,
-    renderBullet: function (index, className) {
-      return `<span class="${className}"></span>`;
-    },
-  };
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+            return `<span class="${className}"></span>`;
+        },
+    };
 
-  return (
-    <Kernel>
-      <div
-        className={styles.brandsGrid}
-        style={{ display: isDesktop ? "grid" : "block" }}
-      >
-        {isDesktop ? (
-          <>
-            {brandsList.map((brand) => (
-              <BrandCard
-                key={brand}
-                brand={brand}
-                content={brandsContent[brand]}
-              />
-            ))}
-          </>
-        ) : (
-          <Swiper
-            slidesPerView="1.2"
-            speed="500"
-            loop="true"
-            spaceBetween="20"
-            className="brandsSwiper"
-            pagination={pagination}
-            modules={[Pagination]}
-          >
-            {brandsList.map((brand) => (
-              <SwiperSlide key={brand} className={styles.swiperSlide}>
-                <BrandCard brand={brand} content={brandsContent[brand]} />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        )}
-      </div>
-    </Kernel>
-  );
+    return (
+        <Kernel>
+            <div
+                className={styles.brandsGrid}
+                style={{ display: isDesktop ? "grid" : "block" }}
+            >
+                {isDesktop ? (
+                    <>
+                        {brandsList.map((brand) => (
+                            <BrandCard
+                                key={brand}
+                                brand={brand}
+                                content={brandsContent[brand]}
+                            />
+                        ))}
+                    </>
+                ) : (
+                    <Swiper
+                        slidesPerView="1.2"
+                        speed="500"
+                        loop="true"
+                        spaceBetween="20"
+                        className="brandsSwiper"
+                        pagination={pagination}
+                        modules={[Pagination, Navigation]}
+                    >
+                        {brandsList.map((brand) => (
+                            <SwiperSlide
+                                key={brand}
+                                className={styles.swiperSlide}
+                            >
+                                <BrandCard
+                                    brand={brand}
+                                    content={brandsContent[brand]}
+                                />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                )}
+            </div>
+        </Kernel>
+    );
 };
 
 export default Brands;
